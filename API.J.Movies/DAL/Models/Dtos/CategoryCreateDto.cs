c8ba07fc8ad5f45d41e9f1ecf59e2c29e8d6512e
LMUV <luisito120127@gmail.com>
2025-11-08 22:20:55 -0500

    AutoMapper create and DTO´s created

---
+++ API.J.Movies/DAL/Models/Dtos/CategoryCreateDto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.OpenApi.MicrosoftExtensions;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.J.Movies.DAL.Models.Dtos
+{
+    public class CategoryCreateDto
+    {
+        [Required(ErrorMessage = "El nombre de la categoria es obligatorio.")]
+        [MaxLength(100, ErrorMessage = "El  numero maximo de caracteres es de 100.")]
+
+        public string Name { get; set; } 
+    }
+}
---
+++ API.J.Movies/MoviesMapper/Mappers.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using API.J.Movies.DAL.Models;
+using API.J.Movies.DAL.Models.Dtos;
+
+namespace API.J.Movies.MoviesMapper
+{
+    public class Mappers : Profile
+    {
+            public Mappers()
+            {
+                // Category Mappings
+                CreateMap<Category, CategoryDto>().ReverseMap();
+                CreateMap<Category, CategoryCreateDto>().ReverseMap();
+
+        }
+    }
+}
